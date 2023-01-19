6d533500673de2abd8a420d0d5dfd5a571464b50
AshwithaG <103858116+AshwithaG@users.noreply.github.com>
2023-01-18 21:04:32 -0600

    Update ICP_2_Program_3.py

--- ICP_2_Program_3.py
+++ ICP_2_Program_3.py
@@ -2,7 +2,7 @@ def inches_to_centimeters():
     cents_list = [150, 155, 145, 148]
     inches_list = []
     for i in cents_list:
-        inches_list.append(float(i * 2.54))
+        inches_list.append(float(i / 2.54))
     print(inches_list)
 
     
